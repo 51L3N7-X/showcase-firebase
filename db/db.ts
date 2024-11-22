5b9db4a36d88bed3c1b5fd3cb17f70fe677aa0dd
51l3n7 <abdullah22shiban42@gmail.com>
2024-11-22 11:38:48 +0300

    fix(data): fixed tmp data for vercel

--- db/db.ts
+++ db/db.ts
@@ -10,17 +10,23 @@ interface Database {
 
 // Determine storage path based on the environment
 const isVercel = process.env.VERCEL || process.env.VERCEL_ENV;
-const writableDir = isVercel ? "/tmp" : path.resolve(process.cwd(), "data"); // Use /tmp on Vercel, 'data' folder locally
-const filePath = path.resolve(writableDir, "burgers.json");
+// const writableDir = isVercel ? "/tmp" : path.resolve(process.cwd(), "data"); // Use /tmp on Vercel, 'data' folder locally
+const filePath = isVercel
+  ? "/tmp/burgers.json"
+  : path.resolve(process.cwd(), "data", "burgers.json");
 
 // Ensure the writable directory exists (useful for local development)
-if (!fs.existsSync(writableDir)) {
-  fs.mkdirSync(writableDir, { recursive: true });
+if (isVercel && !fs.existsSync("/tmp")) {
+  fs.mkdirSync("/tmp", { recursive: true });
 }
+
+const existData = JSON.parse(Buffer.from(
+  fs.readFileSync(path.resolve(process.cwd(), "data", "burgers.json"))
+).toString());
 
 // Check if burgers.json exists (for local development)
 if (!fs.existsSync(filePath)) {
-  fs.writeFileSync(filePath, JSON.stringify({ burgers: [] }, null, 2)); // Initialize with empty data
+  fs.writeFileSync(filePath, JSON.stringify({ burgers: existData.burgers }, null, 2)); // Initialize with empty data
 }
 
 const adapter = new JSONFile<Database>(filePath);
